3dfc62f647ea8b140af2c7ce9dfd1eb177f640f5
Your Name <your_email@example.com>
2024-07-28 14:23:39 +0200

    createProject

--- App1/src/app/app.component.spec.ts
+++ App1/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'session1' title`, () => {
+  it(`should have the 'App1' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('session1');
+    expect(app.title).toEqual('App1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, session1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, App1');
   });
 });
--- App1/src/app/app.component.ts
+++ App1/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'session1';
+  title = 'App1';
 }
