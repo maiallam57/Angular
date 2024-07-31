24bf6cd3960d4da662655867e8c5b58a4d41a755
Your Name <your_email@example.com>
2024-07-31 16:08:24 +0200

    binding

--- App1/src/app/components/home/home.component.ts
+++ App1/src/app/components/home/home.component.ts
@@ -1,43 +1,17 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
     selector: "app-home",
     standalone: true,
-    imports: [],
+    imports: [RouterOutlet, FormsModule],
     templateUrl: "./home.component.html",
     styleUrl: "./home.component.css"
 })
 export default class HomeComponent {
+
+
     userName: string = "Mai Allam";
-    productList: Product[] = [
-        { name: "Toshiba", price: 1234, onSale: true },
-        { name: "Toshiba", price: 1234 }
-    ]
-    productListType: SuperProductType[] = [
-        { name: "Toshiba", price: 1234, onSale: true },
-        { name: "Toshiba", price: 1234 }
-    ]
-
-}
-
-interface Product {
-    name: string,
-    price: number
-}
-
-interface Product {
-    onSale?: boolean,
-    hi?(): string
-}
-
-type ProductType = {
-    name: string,
-    price: number
-}
-type SuperProductType = ProductType & {
-    onSale?: boolean
-}
-
-function returnVar<itsType>(variable: itsType): itsType {
-    return variable;
+    userAge: number = 23;
 }
