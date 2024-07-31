41cfc66031582ab2674b6b77e3b68b024281b046
Your Name <your_email@example.com>
2024-07-31 14:11:01 +0200

    interfaceVsType

--- App1/src/app/components/home/home.component.ts
+++ App1/src/app/components/home/home.component.ts
@@ -9,4 +9,31 @@ import { Component } from "@angular/core";
 })
 export default class HomeComponent {
     userName: string = "Mai Allam";
+    productList: Product[] = [
+        { name: "Toshiba", price: 1234, onSale: true },
+        { name: "Toshiba", price: 1234 }
+    ]
+    productListType: SuperProductType[] = [
+        { name: "Toshiba", price: 1234, onSale: true },
+        { name: "Toshiba", price: 1234 }
+    ]
+
+}
+
+interface Product {
+    name: string,
+    price: number
+}
+
+interface Product {
+    onSale?: boolean,
+    hi?(): string
+}
+
+type ProductType = {
+    name: string,
+    price: number
+}
+type SuperProductType = ProductType & {
+    onSale?: boolean
 }
