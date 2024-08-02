3f4bd2310d66554c3b3bb5249231f69c6040d451
Your Name <your_email@example.com>
2024-08-02 16:34:35 +0200

    ngFor&ngIf

--- App1/src/app/app.component.ts
+++ App1/src/app/app.component.ts
@@ -4,11 +4,12 @@ import HomeComponent from "./components/home/home.component";
 import { AboutComponent } from "./components/about/about.component";
 import { BindingComponent } from './components/binding/binding.component';
 import { ControlFlowComponent } from './components/control-flow/control-flow.component';
+import { NgforNgifComponent } from "./components/ngfor-ngif/ngfor-ngif.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, AboutComponent, BindingComponent, ControlFlowComponent],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, BindingComponent, ControlFlowComponent, NgforNgifComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
