88ca5c425229d6d86bdc6d06a54d1ee2edf7774c
Your Name <your_email@example.com>
2024-08-02 23:21:20 +0200

    product-item

--- App1/src/app/components/control-flow/control-flow.component.ts
+++ App1/src/app/components/control-flow/control-flow.component.ts
@@ -1,10 +1,11 @@
 import { Product } from './../../interfaces/product';
 import { Component } from '@angular/core';
+import { ProductItemComponent } from "../product-item/product-item.component";
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [],
+  imports: [ProductItemComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
@@ -21,5 +22,10 @@ export class ControlFlowComponent {
     { id: 8, name: "Iphone", image: 'assets/images/testimonial-2.jpg', price: 52572, model: 2022, count: 0 },
   ];
 
+  recommenedList: Product[] = [
+    { id: 1, name: "Toshiba", image: 'assets/images/testimonial-2.jpg', price: 2999, model: 2022, count: 1 },
+    { id: 2, name: "Iphone", image: 'assets/images/testimonial-1.jpg', price: 59999, model: 2024, count: 93 },
+    { id: 3, name: "Htc", image: 'assets/images/testimonial-2.jpg', price: 12000, model: 2023, count: 100 },
+  ];
 
 }
